33d2692398a75583204657b40df8753f7c298785
Daria Fediashova <fediashova.it@gmail.com>
2021-04-20 12:50:43 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Дарья Федяшова on 20.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
